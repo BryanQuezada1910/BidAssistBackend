44731b819b3bc0fa10bfed84ffbb0ae2dd088f66
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-06-25 00:13:37 -0600

    Add CORS and Database config

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './src/config/database.js';
+import cors from 'cors';
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Load env vars
+dotenv.config();
+
+// Enable CORS Middleware
+app.use(cors());
+// Body parser
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+// Route get request, to test the server
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
