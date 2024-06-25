9803e0274767c00c88330838b9e2aee816ae11e3
Carlos Gomez <carlosfgr00s@gmail.com>
2024-06-25 13:33:58 -0600

    Cookie parser config

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import { authRouter } from './src/routes/AuthRoutes.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("Connected to DB"))
+  .catch(error => console.error(error));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
