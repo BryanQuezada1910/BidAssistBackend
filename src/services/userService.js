4b09239b293c2492d75d561884f7cf5424c9d095
Carlos Gomez <carlosfgr00s@gmail.com>
2024-06-26 12:22:29 -0600

    Adding token and user service for auth controller

---
+++ src/services/userService.js
@@ -0,0 +1,20 @@
+import User from "../models/User.js"
+import { GenerateRefreshToken } from "../services/tokenAuthService.js";
+
+function addUser(user) {
+    const newUser = new User({ name: user.name, lastname: user.lastname, username: user.username, email: user.email, password: user.password, isSuscribed: false });
+    newUser.save()
+        .then((savedUser) => {
+            const refresh_token = GenerateRefreshToken(savedUser);
+
+            User.findOneAndUpdate(
+                { _id: savedUser._id },
+                { refreshToken: refresh_token },
+                { new: true }
+            ).then(() => console.log(`Add Refresh Token for ${savedUser._id}`));
+        });
+
+    return newUser;
+};
+
+export { addUser };
