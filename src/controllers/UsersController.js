280252fa5ca8fd3600811fdee69eeeead1c66f45
Carlos Gomez <carlosfgr00s@gmail.com>
2024-06-26 17:13:54 -0600

    Initial user controller and router config

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,19 @@
+
+
+const getAllUsers = (req, res) => {
+
+};
+
+const getUserById = (req, res) => {
+
+};
+
+const deleteUser = (req, res) => {
+
+};
+
+const updateUser = (req, res) => {
+
+};
+
+export { getAllUsers, getUserById, deleteUser, updateUser };
