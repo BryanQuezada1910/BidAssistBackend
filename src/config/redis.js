af8ae338ccfae2da757481c356ae456c4d3b5619
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-07-02 21:19:35 -0600

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,35 +6,7 @@ export const createRedisClient = async () => {
   const redisURL = process.env.REDIS_URI;
   try {
     redisClient = createClient({
-      username: process.env.REDIS_USER,
-      password: process.env.REDIS_PASSWORD,
-      socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-        connectTimeout: 40000,
-        tls: true,
-        rejectUnauthorized: false,
-        reconnectStrategy: function (retries) {
-          if (retries > 40) {
-            console.log("Too many attempts to reconnect. Redis connection was terminated");
-            return new Error("Too many retries.");
-          } else {
-            return retries * 500;
-          }
-        }
-      }
-    });
-
-    redisClient.on('error', (err) => {
-      console.error('Redis client error', err);
-    });
-
-    redisClient.on('reconnecting', () => {
-      console.log('Reconnecting to Redis...');
-    });
-
-    redisClient.on('end', () => {
-      console.log('Disconnected from Redis');
+      url: process.env.REDIS_URL
     });
 
     // connect to the Redis server
