be311874bd20652acdade847005310b3214cff0e
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-07-02 23:09:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,80 +1,69 @@
-import dotenv from "dotenv"; // Import dotenv module
-import { createRedisClient } from "./src/config/redis.js";
-
-// Load env vars
-dotenv.config();
-await createRedisClient();
-
-import express from "express"; // Import express module
-import cors from "cors"; // Import cors module
-import cookieParser from 'cookie-parser'; // Import cookie-parser module
-import http from 'http'; // Import http module for creating server instance
-import { Server } from "socket.io"; // Import Server class from socket.io
+// Importación de módulos
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server } from "socket.io";
 import compression from "compression";
 import helmet from "helmet";
-
-import socketHandler from "./src/websocket/socketHandler.js"; // Import socketHandler function
-import connectDB from "./src/config/database.js"; // Import connectDB function
-
-import { authRouter } from './src/routes/authRoutes.js'; // Import auth routes
-import { auctionRouter } from './src/routes/auctionRoutes.js'; // Import auction routes
+import socketHandler from "./src/websocket/socketHandler.js";
+import connectDB from "./src/config/database.js";
+import { authRouter } from './src/routes/authRoutes.js';
+import { auctionRouter } from './src/routes/auctionRoutes.js';
 import { usersRouter } from "./src/routes/userRoutes.js";
-import { ticketsRouter } from "./src/routes/ticketRoutes.js"; // Import ticket routes
-import { webHookRouter } from "./src/routes/webHookRoutes.js"; // 
+import { ticketsRouter } from "./src/routes/ticketRoutes.js";
+import { webHookRouter } from "./src/routes/webHookRoutes.js";
 import { reportRouter } from "./src/routes/reportRoutes.js";
 
+// Configuración de variables de entorno
+dotenv.config();
 
+// Creación de la aplicación Express
 const app = express();
-const server = http.createServer(app); // Create server instance
+const server = http.createServer(app);
+
+// Configuración de Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "*",// Allow the client to connect, must be changed in production
-    methods: ["GET", "POST"], // Allow GET and POST requests
+    origin: 'http://localhost:4200', // Cambiar al dominio real en producción
+    methods: ["GET", "POST"],
+    credentials: true // Habilita el uso de credenciales
   }
-}); // Create socket.io instance
+});
 
-// Connect to database
+// Conexión a la base de datos
 connectDB();
 
+// Middleware
+app.use(compression()); // Compresión para mejorar el rendimiento
+app.use(helmet()); // Mejoras de seguridad
+app.use(cors({
+  origin: 'http://localhost:4200', // Cambiar al dominio real en producción
+  methods: ['GET', 'POST'],
+  credentials: true // Habilita el uso de credenciales (cookies, tokens, etc.)
+}));
+app.use(express.json()); // Parseo del body de las peticiones como JSON
+app.use(cookieParser()); // Middleware para el manejo de cookies
 
-const corsOptions = {
-  origin: '*', // Origen de tu aplicación Angular
-  credentials: true, // Permitir el uso de credenciales
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-};
-
-
-app.use(compression());
-// app.use(helmet())
-// Enable CORS Middleware
-app.use(cors(corsOptions));
-// Body parser
-app.use(express.json());
-// Cookie parser
-app.use(cookieParser());
-
-
-// Auth routes
+// Rutas
 app.use('/api/auth', authRouter);
-
 app.use('/api/user', usersRouter);
-// Ticket routes
-app.use("/api/ticket", ticketsRouter);
-app.use("api/webhook", webHookRouter)
-// Auction routes
+app.use('/api/ticket', ticketsRouter);
+app.use('/api/webhook', webHookRouter);
 app.use('/api/auction', auctionRouter);
-app.use("/api/report", reportRouter);
+app.use('/api/report', reportRouter);
 
-// Route get request, to test the server
+// Ruta de prueba
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// Initialize WebSocket handler
+// Inicialización del manejo de WebSocket
 socketHandler(io);
 
-// Start the server
+// Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
